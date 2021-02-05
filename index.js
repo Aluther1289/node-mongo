893ea7d865d31310986f99084a4e03e2856e3481
Adam Luther <aluther1289@gmail.com>
2021-02-04 19:51:29 -0500

    Node Callback Hell and Promises

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert").strict;
+const dboper = require("./operations");
 
 const url = "mongodb://localhost:27017/";
 const dbname = "nucampsite";
@@ -11,25 +12,54 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
 
 	const db = client.db(dbname);
 
-	db.dropCollection("campsites", (err, result) => {
-		assert.strictEqual(err, null);
-		console.log("Dropped Collection", result);
+	db.dropCollection("campsites")
+		.then((result) => {
+			console.log("Dropped Collection:", result);
+		})
+		.catch((err) => console.log("No collection to drop."));
 
-		const collection = db.collection("campsites");
-
-		collection.insertOne(
+	dboper
+		.insertDocument(
+			db,
 			{ name: "Breadcrumb Trail Campground", description: "Test" },
-			(err, result) => {
-				assert.strictEqual(err, null);
-				console.log("Insert Document:", result.ops);
+			"campsites"
+		)
+		.then((result) => {
+			console.log("Insert Document:", result.ops);
 
-				collection.find().toArray((err, docs) => {
-					assert.strictEqual(err, null);
-					console.log("Found Documents:", docs);
+			return dboper.findDocuments(db, "campsites");
+		})
+		.then((docs) => {
+			console.log("Found Documents:", docs);
 
-					client.close();
-				});
-			}
-		);
-	});
-});
+			return dboper.updateDocument(
+				db,
+				{ name: "Breadcrumb Trail Campground" },
+				{ description: "Updated Test Description" },
+				"campsites"
+			);
+		})
+		.then((result) => {
+			console.log("Updated Document Count:", result.result.nModified);
+
+			return dboper.findDocuments(db, "campsites");
+		})
+		.then((docs) => {
+			console.log("Found Documents:", docs);
+
+			return dboper.removeDocument(
+				db,
+				{ name: "Breadcrumb Trail Campground" },
+				"campsites"
+			);
+		})
+		.then((result) => {
+			console.log("Deleted Document Count:", result.deletedCount);
+
+			return client.close();
+		})
+		.catch((err) => {
+			console.log(err);
+			client.close();
+		});
+}).catch((err) => console.log(err));
